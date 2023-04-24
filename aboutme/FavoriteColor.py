2bc4b50f7213011d2d55e3fc4e2070648e384265
Will Torman <willtorman@gmail.com>
2023-04-24 09:55:49 -0600

    I saved files

---
+++ aboutme/FavoriteColor.py
@@ -0,0 +1,8 @@
+favorite_color = input("What is your favorite color?")
+print("Your favorite color is", favorite_color)
+
+# response to the users favorite color
+print("Your favorite color is " + favorite_color)
+
+# ask them why that is their favorite color, because I like a good conversation
+print("Why is " + favorite_color + " your favorite color?")
